56f87ec3f81c51f8d7a75a9d7c0e3f9802242ef3
GitHub Action <action@github.com>
2025-05-26 17:27:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/allicai/sweeptakehome/services/MyProjectService.kt
+++ src/main/kotlin/com/github/allicai/sweeptakehome/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.allicai.sweeptakehome.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.allicai.sweeptakehome.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
